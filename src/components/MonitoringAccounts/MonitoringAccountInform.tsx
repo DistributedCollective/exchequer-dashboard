5f4949feaefb0e8cd6d3ec564f881589ac163ccf
Shamshin Alex <shamshin.alexandr@gmail.com>
2021-08-02 11:28:36 +0300

    added dialog for account vote - updated btn hover style

--- src/components/MonitoringAccounts/MonitoringAccountInform.tsx
+++ src/components/MonitoringAccounts/MonitoringAccountInform.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Button } from '../Button';
 import iconClose from '../../assets/icon-close.svg';
+import { Dialog } from '../Dialog';
 
 //todo
 //update props
@@ -12,6 +13,21 @@ interface InformProps {
 
 export function MonitoringAccountInform(props: InformProps) {
   const [visible, setVisible] = useState(true);
+  const closeInform = useCallback(() => {
+    setVisible(prevState => !prevState);
+  }, []);
+  const [open, setOpen] = useState(false);
+  const toggleDialog = useCallback(() => {
+    setOpen(prevState => !prevState);
+  }, []);
+  const approveAccount = useCallback(() => {
+    setOpen(false);
+    setVisible(false);
+  }, []);
+  const agaistAccount = useCallback(() => {
+    setOpen(false);
+    setVisible(false);
+  }, []);
   return (
     <>
       {visible && (
@@ -21,23 +37,46 @@ export function MonitoringAccountInform(props: InformProps) {
               className="p-1 w-6 absolute right-3 top-3"
               title="Close"
               text={<img src={iconClose} alt="close" />}
-              onClick={() => setVisible(false)}
+              onClick={closeInform}
             />
-            <h3 className="text-xl font-semibold leading-6 mb-6">
+            <h3 className="text-xl font-semibold leading-6 mb-6 pr-4 sm:pr-0">
               New Account Has Been Added
             </h3>
             <p className="mb-3">XXXXXXXXX add an account named:</p>
             <ul>
               <li>- {props.accountName}</li>
-              <li>- {props.accountAddress}</li>
+              <li className="whitespace-nowrap overflow-hidden overflow-ellipsis">
+                - {props.accountAddress}
+              </li>
             </ul>
             <Button
               text="Approve"
               primary
-              className="mt-8"
-              onClick={() => setVisible(false)}
+              className="mt-8 w-full sm:w-auto"
+              onClick={toggleDialog}
             />
           </div>
+
+          <Dialog isOpen={open} onClose={() => setOpen(false)}>
+            <div className="text-center">
+              <span>Approve account</span>
+              <div className="flex items-center justify-center mt-10">
+                <Button
+                  text="Approve"
+                  className="mr-3"
+                  primary
+                  onClick={approveAccount}
+                />
+                <Button
+                  text="Agains"
+                  className="mr-3"
+                  primary
+                  onClick={agaistAccount}
+                />
+                <Button text="Close" primary onClick={() => setOpen(false)} />
+              </div>
+            </div>
+          </Dialog>
         </div>
       )}
     </>
